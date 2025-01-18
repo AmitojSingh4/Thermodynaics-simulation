b453f58deec3e496cfdc41fde507982530efd710
Amitoj <amitojsingh4153@gmail.com>
2025-01-18 19:46:50 +0000

    commit 5
    Updated many functions to take the array as a variable so they can work with the temporary arrays.
    Added a function to flip a random element and save it to a a temporary array.
    Added a function to calculate the change in energy before and after the flip.

--- Main.R
+++ Main.R
@@ -1,5 +1,5 @@
 
-CreateArray <- function(n){
+CreateArray <- function(n){ #Creates array of size nxn 
   A <- array(Q, dim = c(n, n))
   for (i in 1:n) {
     for (j in 1:n) {
@@ -10,46 +10,76 @@ CreateArray <- function(n){
   return(A)
 }
 
-Epsilon <- function(i,j) { #Energy of a single atom
-  energy <- -J*(A[i,j]*(GetValue(i+1,j)+GetValue(i-1,j)+GetValue(i,j+1)+GetValue(i,j-1)))
+Epsilon <- function(i,j,array) { #Energy of a single element
+  if ((0<i&i<n)&(0<j&j<n)) {
+  energy <- -J*(array[i,j]*(GetValue(i+1,j,array)+GetValue(i-1,j,array)+GetValue(i,j+1,array)+GetValue(i,j-1,array)))
+  }
+  else {
+    energy <- 0
+  }
   return(energy)
 }
 
-GetValue <- function(a,b) { #Sanitation for edge cases
+GetValue <- function(a,b,array) { #Sanitation for edge cases
   if ((0<a&a<n)&(0<b&b<n)) {
-    return(A[a,b])
+    return(array[a,b])
   }
   else {
     return(0)
   }
 }
 
-E <- function() { #Total energy of the entire lattice
+E <- function(array) { #Total energy of the entire lattice
   E <- 0
   for (i in 1:n) {
     for (j in 1:n) {
-      E <- E + Epsilon(i,j)
+      E <- E + Epsilon(i,j,array)
     }
   }
   return(E)
 }
 
-M <- function() { #Total magnetization of the lattice
+M <- function(array) { #Total magnetization of the lattice
   M <- 0
   for (i in 1:n) {
     for (j in 1:n) {
-      M <- M + A[i,j]
+      M <- M + array[i,j]
     }
   }
   return(M)
 }
 
+Flip <- function() { #Flips the spin of a random element
+  i <- sample(c(1:n),size = 1)
+  j <- sample(c(1:n),size = 1)
+  ATemp <<- A
+  if (A[i,j]==1) {
+    ATemp[i,j] <<- -1
+  }
+  else {
+    ATemp[i,j] <<- 1
+  }
+  return(array(c(i,j), dim = c(1,2)))
+}
+
+DeltaE <- function() {
+  EBefore <- E(A)
+  FlipData <- Flip()
+  FEI <- FlipData[1,1] #Flipped Element I
+  FEJ <- FlipData[1,2] #Flipped Element J
+  LocalEBefore <- Epsilon(FEI,FEJ,A) + Epsilon(FEI+1,FEJ,A) + Epsilon(FEI-1,FEJ,A) + Epsilon(FEI,FEJ+1,A) + Epsilon(FEI,FEJ-1,A)
+  LocalEAfter <- Epsilon(FEI,FEJ,ATemp) + Epsilon(FEI+1,FEJ,ATemp) + Epsilon(FEI-1,FEJ,ATemp) + Epsilon(FEI,FEJ+1,ATemp) + Epsilon(FEI,FEJ-1,ATemp)
+  EAfter <- EBefore - LocalEBefore + LocalEAfter
+  dE <- EAfter - EBefore
+  return(dE)
+}
+
 Main <- function() {
   n <<- 10 #Size of lattice (n x n)
-  J <<- 1
+  J <<- 1 #Coupling constant
   A <<- CreateArray(n)
-  print(E())
-  print(M())
+  image(A)
+  print(DeltaE())
 }
 
 Main()
