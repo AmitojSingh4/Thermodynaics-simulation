11647f52dc7a9a6dc3702a3235fd6cea38df7a69
Amitoj <amitojsingh4153@gmail.com>
2025-01-18 16:05:00 +0000

    Create Main.R
    Created lattice as an array
    Computing energy of a single particle

---
+++ Main.R
@@ -0,0 +1,19 @@
+# size of lattice (n x n)
+n <- 10
+
+# number of quanta / pixel (integer)
+Q <- 1
+
+# make initial lattice
+A <- array(Q, dim = c(n, n))
+
+J <- 1
+
+
+Energy <- function(i,j) {
+  A[i]
+}
+
+x <- array(1:20, dim=c(4,5))
+print (x)
+print(x[4,2])
